7d2085c70e82931092ed6e765dda8c3508c329b6
Jure Potocnik <pot.jure@gmail.com>
2026-03-23 19:01:35 +0100

    Add interactive floating-islands sandbox upgrade

--- js/game/core/Island.js
+++ js/game/core/Island.js
@@ -1,113 +1,141 @@
+var Island = function (options) {
+  this.name = options.name;
+  this.x = options.x;
+  this.z = options.z;
+  this.baseAltitude = options.baseAltitude;
+  this.footprintRadius = options.footprintRadius;
+  this.heightLevels = options.heightLevels;
+  this.driftPhase = options.driftPhase || 0;
+  this.rotationSpeed = options.rotationSpeed || 0.0025;
+  this.blockCount = 0;
+  this.interactiveMeshes = [];
+  this.cubes = [];
 
-var Island = function(x, y, z, size)
-{
-	this.nCubes = 10;  // Number of graphical cubes in this logical cube on each side
-	
-	
-	this.x = x;
-	this.y = y;
-	this.z = z;
-	
-	this.size = size;
-	
-	this.colors =
-	[
-		0x3ba606,
-		0x51a606,
-		0x3b7e00,
-		0x4b9013,
-		0x3b7e00,
-		0x4b9013,
-		0x51a606,
-		0x3ba606,
-		0x34880b
-	];
-	
-	
-	
-	//////
-	// Make the logical cubes of this island
-	//////
-	
-	this.smallCubeSize = this.size / this.nCubes;
-	
-	this.cubes = [];
-	
-	// 9 cubes on 3 y levels
-	/*
-	for (var i = 0; i < this.nCubes * this.nCubes * this.nCubes; i++)
-	{
-		// The starting pos
-		var sX = -this.size / 2;
-		var sY = -this.size / 2;
-		var sZ = -this.size / 2;
-		
-		this.cubes.push(
-			new Cube(
-				sX  +  ( this.smallCubeSize / 2 )  +
-					( i % this.nCubes ) * ( this.smallCubeSize ),
-				sY  +  ( this.smallCubeSize / 2 )  +
-					( Math.floor( i / ( this.nCubes * this.nCubes ) ) ) * ( this.smallCubeSize ),
-				sZ  +  ( this.smallCubeSize / 2 )  +
-					( ( Math.floor( i / this.nCubes ) % this.nCubes ) ) * ( this.smallCubeSize ),
-				this.smallCubeSize,
-				this.colors[ Math.floor( ( Math.random() * (this.colors.length - 1) ) + 1 ) ]
-			)
-		);
-	}
-	*/
-	
-	
-	//////
-	// Make a cube
-	//////
-	
-	var geometry = new THREE.BoxGeometry(
-		this.size,
-		this.size,
-		this.size
-	);
-	
-	var materials = [];
-	for (var i = 0; i < 6; i++)
-	{
-		//var texture = THREE.ImageUtils.loadTexture( "assets/textures/cube/cube-surface-" + (i + 1) + ".png" );
-		var texture = THREE.ImageUtils.loadTexture( "assets/textures/cube/cube-block-1.png" );
-		texture.magFilter = THREE.NearestFilter;
-		texture.minFilter = THREE.NearestFilter;
-		
-		materials.push(
-			new THREE.MeshBasicMaterial(
-				{
-					//wireframe: (i%2==0 ? false : true),
-					//wireframe: true,
-					map: texture
-				}
-			)
-		);
-	}
-	
-	this.cube = new THREE.Mesh(
-		geometry,
-		new THREE.MeshFaceMaterial( materials )
-	);
-	
-	this.cube.translateX( this.x );
-	this.cube.translateY( this.y );
-	this.cube.translateZ( this.z );
-	
-	Game.scene.add( this.cube );
+  this.palette = Island.getPalette(options.colorSeed || 0);
+  this.group = new THREE.Object3D();
+  this.group.position.set(this.x, this.baseAltitude, this.z);
+
+  this.selectionRing = new THREE.Mesh(
+    new THREE.TorusGeometry(this.footprintRadius * 2.7, 0.5, 10, 40),
+    new THREE.MeshBasicMaterial({
+      color: 0xffe3a8,
+      transparent: true,
+      opacity: 0.85
+    })
+  );
+  this.selectionRing.rotation.x = Math.PI / 2;
+  this.selectionRing.position.y = -1.5;
+  this.selectionRing.visible = false;
+  this.group.add(this.selectionRing);
+
+  this.buildTerrain();
+
+  this.description = "Altitude " + this.baseAltitude.toFixed(1) + ", " +
+    this.heightLevels + " terrain layers, " + this.blockCount + " total blocks.";
 };
 
-Island.prototype.update = function()
-{
-	// Animation
-	//this.cube.rotation.y += 0.005;
-	
-	/*
-	for (var i = 0; i < this.nCubes * this.nCubes * this.nCubes; i++)
-	{
-		this.cubes[i].update();
-	}
-	*/
+Island.getPalette = function (seed) {
+  var palettes = [
+    {
+      top: 0x7fbf63,
+      side: 0x6f8d4a,
+      dirt: 0x8c6941,
+      rock: 0x80858a
+    },
+    {
+      top: 0x79b56b,
+      side: 0x6b8462,
+      dirt: 0x876145,
+      rock: 0x727880
+    },
+    {
+      top: 0x93c16d,
+      side: 0x6d9b57,
+      dirt: 0xa27647,
+      rock: 0x7e7772
+    }
+  ];
+
+  return palettes[seed % palettes.length];
+};
+
+Island.prototype.buildTerrain = function () {
+  var layer;
+  var x;
+  var z;
+  var size = 4;
+
+  for (layer = 0; layer < this.heightLevels; layer++) {
+    var radius = this.footprintRadius - layer * 0.7;
+    var max = Math.ceil(radius);
+
+    for (x = -max; x <= max; x++) {
+      for (z = -max; z <= max; z++) {
+        var radialDistance = Math.sqrt(x * x + z * z);
+        var noise = Math.sin((x + this.driftPhase) * 1.17) + Math.cos((z - this.driftPhase) * 1.11);
+        var threshold = radius + noise * 0.2;
+
+        if (radialDistance > threshold) {
+          continue;
+        }
+
+        var cubeType = layer === this.heightLevels - 1 ? "top" : (layer === 0 ? "rock" : "dirt");
+        var cube = new Cube(
+          x * size,
+          layer * size,
+          z * size,
+          size,
+          {
+            top: this.palette.top,
+            side: cubeType === "rock" ? this.palette.rock : this.palette.side,
+            bottom: this.palette.dirt
+          },
+          cubeType
+        );
+
+        cube.object.userData.island = this;
+        this.cubes.push(cube);
+        this.interactiveMeshes.push(cube.object);
+        this.group.add(cube.object);
+        this.blockCount++;
+      }
+    }
+  }
+
+  this.addLanterns();
+};
+
+Island.prototype.addLanterns = function () {
+  var glowGeometry = new THREE.SphereGeometry(0.8, 10, 10);
+  var glowMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffdd9b,
+    transparent: true,
+    opacity: 0.8
+  });
+  var i;
+
+  for (i = 0; i < 3; i++) {
+    var glow = new THREE.Mesh(glowGeometry, glowMaterial.clone());
+    glow.position.set(
+      Game.randomRange(-this.footprintRadius * 2, this.footprintRadius * 2),
+      this.heightLevels * 4 + Game.randomRange(2, 7),
+      Game.randomRange(-this.footprintRadius * 2, this.footprintRadius * 2)
+    );
+    var scale = Game.randomRange(0.7, 1.4);
+    glow.scale.set(scale, scale, scale);
+    this.group.add(glow);
+  }
+};
+
+Island.prototype.setSelected = function (isSelected) {
+  this.selectionRing.visible = isSelected;
+};
+
+Island.prototype.update = function (elapsed, animationEnabled) {
+  var bob = Math.sin(elapsed * 0.9 + this.driftPhase) * 1.8;
+  this.group.position.y = this.baseAltitude + (animationEnabled ? bob : 0);
+
+  if (animationEnabled) {
+    this.group.rotation.y += this.rotationSpeed;
+  }
 };
--- js/game/core/Island/Cube.js
+++ js/game/core/Island/Cube.js
@@ -1,60 +1,25 @@
+var Cube = function (x, y, z, size, palette, cubeType) {
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  this.size = size;
+  this.palette = palette;
+  this.cubeType = cubeType || "dirt";
 
-var Cube = function(x, y, z, size, color)
-{
-	this.x = x;
-	this.y = y;
-	this.z = z;
-	
-	this.size = size;
-	
-	this.color = color;
-	
-	
-	//////
-	// Make a cube
-	//////
-	
-	var geometry = new THREE.BoxGeometry(
-		this.size,
-		this.size,
-		this.size
-	);
-	
-	var materials = [];
-	for (var i = 0; i < 6; i++)
-	{
-		/*
-		var texture = THREE.ImageUtils.loadTexture( "assets/textures/cube/cube-surface-" + (i + 1) + ".png" );
-		texture.magFilter = THREE.NearestFilter;
-		texture.minFilter = THREE.NearestFilter;
-		*/
-		
-		materials.push(
-			new THREE.MeshBasicMaterial(
-				{
-					//wireframe: true,
-					color: this.color,
-					//map: texture
-				}
-			)
-		);
-	}
-	
-	this.cube = new THREE.Mesh(
-		geometry,
-		new THREE.MeshFaceMaterial( materials )
-	);
-	
-	this.cube.translateX( this.x );
-	this.cube.translateY( this.y );
-	this.cube.translateZ( this.z );
-	
-	Game.scene.add( this.cube );
+  var geometry = new THREE.BoxGeometry(this.size, this.size, this.size);
+  var materials = [
+    new THREE.MeshLambertMaterial({ color: this.palette.side }),
+    new THREE.MeshLambertMaterial({ color: this.palette.side }),
+    new THREE.MeshLambertMaterial({ color: this.cubeType === "top" ? this.palette.top : this.palette.side }),
+    new THREE.MeshLambertMaterial({ color: this.palette.bottom }),
+    new THREE.MeshLambertMaterial({ color: this.palette.side }),
+    new THREE.MeshLambertMaterial({ color: this.palette.side })
+  ];
+
+  this.object = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+  this.object.position.set(this.x, this.y, this.z);
 };
 
-Cube.prototype.update = function()
-{
-	this.cube.position.x = this.x;
-	this.cube.position.y = this.y;
-	this.cube.position.z = this.z;
+Cube.prototype.update = function () {
+  this.object.position.set(this.x, this.y, this.z);
 };
